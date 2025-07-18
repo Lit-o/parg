eea49fe7aa78209369c30b242bacd33900e55f8b
Boris Lito <boris12lit@gmail.com>
2025-07-18 21:43:06 +0300

    add LoadingOverlay and set up color type and props

--- src/shared/components/LoadingOverlay.tsx
+++ src/shared/components/LoadingOverlay.tsx
@@ -1,23 +1,27 @@
 import { StyleSheet, View, ActivityIndicator } from 'react-native';
-import COLORS from '../const/Colors';
+import { ColorType } from '../const/Colors';
+
+interface propsType {
+    color: ColorType
+}
 
 
-const LoadingOverlay = () => {
-  return (
-    <View style={styles.container}>
-      <ActivityIndicator size='large' color={COLORS.SYSTEM.WAITING} />
-    </View>
-  );
+const LoadingOverlay = ({ color }: propsType) => {
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator size='large' color={color} />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-    backgroundColor: 'transparent'
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: 'transparent'
+    },
 })
 
 export default LoadingOverlay;
