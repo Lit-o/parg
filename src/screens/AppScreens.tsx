6f93867e676a88556b302e0ab9d361284f5af541
Boris Lito <boris12lit@gmail.com>
2025-07-29 02:56:13 +0300

    add first kodify-nano counter

--- src/screens/AppScreens.tsx
+++ src/screens/AppScreens.tsx
@@ -1,10 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import LogIn from './LogIn'
 
 export default function AppScreens() {
+    const [counter, setCounter] = useState(0)
+
     return (
         <View style={styles.container}>
+            <Text>Counter: {counter}</Text>
             <LogIn />
         </View>
     )
