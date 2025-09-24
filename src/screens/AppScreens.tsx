35e19462773dbe06f314a008ff9980e66ce0f9d8
Boris Lito <boris12lit@gmail.com>
2025-09-24 20:18:19 +0300

    add LOGIN-image and styles to login

--- src/screens/AppScreens.tsx
+++ src/screens/AppScreens.tsx
@@ -2,15 +2,19 @@ import { View, Text, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 import LogIn from './LogIn'
 
-export default function AppScreens() {
 
+
+export default function AppScreens() {
     const [isSignedIn, setIsSignedIn] = useState(false)
 
     return (
         <View style={styles.container}>
-            {isSignedIn ? <Text style={styles.loginText}>Login</Text> :
+            {isSignedIn ? 
+                <Text>
+                    After login placeholder
+                </Text> 
+                :
                 <>
-                    <Text style={styles.loginText}>Sign Up</Text>
                     <LogIn onLogin={() => setIsSignedIn(true)} />
                 </>
             }
@@ -22,10 +26,5 @@ export default function AppScreens() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    },
-    loginText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: '#333',
-    },
+    }
 });
--- src/screens/LogIn.tsx
+++ src/screens/LogIn.tsx
@@ -1,12 +1,15 @@
-import { StyleSheet, View } from 'react-native'
+import { ImageBackground, StyleSheet, View } from 'react-native'
 import { Text, TextInput } from '../shared/hoc/WithFontAutoScaleOff'
 import React, { useState } from 'react'
 import COLORS from '../shared/const/Colors'
 import LoadingOverlay from '../shared/components/LoadingOverlay'
 
+
+
 interface LogInProps {
     onLogin: (username: string, password: string) => void;
 }
+
 
 const LogIn: React.FC<LogInProps> = ({ onLogin }) => {
     const [isLoading, setIsLoading] = useState(false);
@@ -26,43 +29,78 @@ const LogIn: React.FC<LogInProps> = ({ onLogin }) => {
         }
     };
     return (
-        <View style={styles.container}>
-            <Text style={{ fontSize: 30, color: COLORS.TEXT.terracottaSoot }}>LogIn</Text>
-            <TextInput style={styles.textInput}/>
-            {/* <LoadingOverlay color={COLORS.COMMON.FIRE_RUST}/> */}
-            {/* <LoadingOverlay color={'#5D7A9E'}/> */}
-            <TextInput
-                style={styles.textInput}
-                placeholder="Username"
-                value={username}
-                onChangeText={setUsername}
-            />
-            <TextInput
-                style={styles.textInput}
-                placeholder="Password"
-                secureTextEntry
-                value={password}
-                onChangeText={setPassword}
-            />
+        <ImageBackground
+            source={require('../shared/assets/images/LOGIN.jpg')}
+            style={styles.backgroundImage}
+            blurRadius={2}>
+            <View style={styles.glassCard}>
+                <Text style={{ fontSize: 30, color: COLORS.TEXT.terracottaSoot }}>Login Page</Text>
+                {/* <LoadingOverlay color={COLORS.COMMON.FIRE_RUST}/> */}
+                {/* <LoadingOverlay color={'#5D7A9E'}/> */}
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Username"
+                    value={username}
+                    onChangeText={setUsername}
+                />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="First Password"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+                />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Second Password"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+                />
+            </View>
+
+
             {isLoading ? (
                 <LoadingOverlay color={COLORS.COMMON.FIRE_RUST} />
             ) : null}
-        </View>
+
+
+        </ImageBackground>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 20,
-        flex: 1,
-        backgroundColor: COLORS.COMMON.BURNT_GOLD
+
     },
     textInput: {
+        width: 260,
+        paddingHorizontal: 12,
         marginTop: 10,
         borderWidth: 2,
         borderRadius: 8,
         borderColor: COLORS.COMMON.WASHED_TEAL
-    }
+    },
+    backgroundImage: {
+        padding: 20,
+        flex: 1,
+        backgroundColor: COLORS.COMMON.BURNT_GOLD,
+        // height: 244,
+        resizeMode: 'cover',
+        alignItems: 'center'
+    },
+    glassCard: {
+        // margin: 'auto',
+        marginTop: 40,
+        width: 300,
+        height: 400,
+        // backgroundColor: COLORS.COMMON.BURNT_GOLD + `DD`,
+        backgroundColor: COLORS.COMMON.BURNT_GOLD,
+        borderRadius: 20,
+
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
 
 export default LogIn;
